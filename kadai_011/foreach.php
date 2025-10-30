8e56c3836db8ab923a4d102def11f4c28725d05d
asamispies <miii_000_a@icloud.com>
2025-10-30 18:02:42 +0900

    kadai_011 upload
    kadai_011 upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>課題011です</title>
+</head>
+<body>
+  <p>
+    <?php
+    $foods = ['名前' =>'玉ねぎ', '値段' => 200, '産地' => '北海道'];
+    foreach($foods as $key => $value) {
+      echo "{$key}： {$value}<br>";
+    }
+    ?>
+    </p>
+  
+</body>
+</html>
